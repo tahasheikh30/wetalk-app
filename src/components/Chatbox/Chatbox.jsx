0dee8eea40ade65ae80174d42c452fea66198cf5
Taha <tahasheikh30@gmail.com>
2024-10-20 00:35:50 +0500

    Sending image functionality added

--- src/components/Chatbox/Chatbox.jsx
+++ src/components/Chatbox/Chatbox.jsx
@@ -11,6 +11,7 @@ import {
 } from "firebase/firestore";
 import { db } from "../../config/Firebase.js";
 import { toast } from "react-toastify";
+import upload from "../../library/Upload.js";
 
 function Chatbox() {
   const { userData, messagesId, chatUser, messages, setMessages } =
@@ -57,17 +58,56 @@ function Chatbox() {
     setInput("");
   };
 
+  const sendImage = async (e) => {
+    try {
+      const fileUrl = await upload(e.target.files[0]);
+
+      if (fileUrl && messagesId) {
+        await updateDoc(doc(db, "messages", messagesId), {
+          messages: arrayUnion({
+            sId: userData.id,
+            image: fileUrl,
+            createdAt: new Date(),
+          }),
+        });
+
+        const userIDs = [chatUser.rId, userData.id];
+
+        userIDs.forEach(async (id) => {
+          const userChatsRef = doc(db, "chats", id);
+          const userChatsSnapshot = await getDoc(userChatsRef);
+
+          if (userChatsSnapshot.exists()) {
+            const userChatData = userChatsSnapshot.data();
+            const chatIndex = userChatData.chatsData.findIndex(
+              (c) => c.messageId === messagesId
+            );
+            userChatData.chatsData[chatIndex].lastMessage = "Image";
+            userChatData.chatsData[chatIndex].updatedAt = Date.now();
+            if (userChatData.chatsData[chatIndex].rId === userData.id) {
+              userChatData.chatsData[chatIndex].messageSeen = false;
+            }
+            await updateDoc(userChatsRef, {
+              chatsData: userChatData.chatsData,
+            });
+          }
+        });
+      }
+    } catch (error) {
+      toast.error(error.message);
+    }
+  };
+
   const convertTimestamp = (timestamp) => {
     let date = timestamp.toDate();
     const hour = date.getHours();
     const minute = date.getMinutes();
     if (hour > 12) {
       return hour - 12 + ":" + minute + " PM";
-    }
-    else{
+    } else {
       return hour + ":" + minute + " AM";
     }
-  }
+  };
 
   // Handle key press event for the Enter key
   const handleKeyPress = (e) => {
@@ -100,12 +140,27 @@ function Chatbox() {
 
       <div className="chat-message">
         {messages.map((message, index) => (
-          <div key={index} className={message.sId === userData.id ? "s-msg" : "r-msg"}>
-            <p className="message">
-              {message.text}
-            </p>
+          <div
+            key={index}
+            className={message.sId === userData.id ? "s-msg" : "r-msg"}
+          >
+            {message["image"] ? (
+              <a href={message.image} target="_blank" rel="noopener noreferrer">
+                <img className="message-image" src={message.image} alt="" />
+              </a>
+            ) : (
+              <p className="message">{message.text}</p>
+            )}
+
             <div>
-              <img src={message.sId === userData.id ? userData.avatar : chatUser.userData.avatar} alt="" />
+              <img
+                src={
+                  message.sId === userData.id
+                    ? userData.avatar
+                    : chatUser.userData.avatar
+                }
+                alt=""
+              />
               <p>{convertTimestamp(message.createdAt)}</p>
             </div>
           </div>
@@ -120,7 +175,13 @@ function Chatbox() {
           type="text"
           placeholder="Type a message..."
         />
-        <input type="file" id="image" accept="image/png, image/jpeg" hidden />
+        <input
+          onChange={sendImage}
+          type="file"
+          id="image"
+          accept="image/png, image/jpeg"
+          hidden
+        />
         <label htmlFor="image">
           <img src={assets.gallery_icon} alt="" />
         </label>
