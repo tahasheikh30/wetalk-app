b545c9c2d6bdba7cbb54669b763ca0e36b626301
Taha <tahasheikh30@gmail.com>
2024-10-23 16:59:15 +0500

    Assets file updated

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -1,12 +1,7 @@
 import gallery_icon from './gallery_icon.png'
 import help_icon from './help_icon.png'
 import logo_icon from './logo_icon.png'
-import logo_big from './logo_big.png'
 import logo from './logo.png'
-import pic1 from './pic1.png'
-import pic2 from './pic2.png'
-import pic3 from './pic3.png'
-import pic4 from './pic4.png'
 import profile_img from './profile_richard.png'
 import search_icon from './search_icon.png'
 import send_button from './send_button.png'
@@ -18,13 +13,8 @@ import green_dot from './green_dot.png'
 const assets = {
     gallery_icon,
     help_icon,
-    logo_big,
     logo_icon,
     logo,
-    pic1,
-    pic2,
-    pic3,
-    pic4,
     profile_img,
     search_icon,
     send_button,
